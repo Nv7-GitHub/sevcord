b393c8c3d5ddbc9dbc6075b465bae3de4a60daed
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2022-08-12 12:08:02 -0700

    Roles

--- discord.go
+++ discord.go
@@ -1,13 +1,90 @@
 package sevcord
 
 import (
+	"errors"
 	"image"
 	"io"
+	"sort"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
+// Color represents a discord color, and is the decimal representation of a hex color. For example: blue would be `Color(0x0000FF)`
+type Color int
+
+// Permissions
+type Permissions int64
+
+const (
+	// Text channel permissions
+	PermissionViewChannel           = Permissions(discordgo.PermissionViewChannel)
+	PermissionSendMessages          = Permissions(discordgo.PermissionSendMessages)
+	PermissionSendTTSMessages       = Permissions(discordgo.PermissionSendTTSMessages)
+	PermissionManageMessages        = Permissions(discordgo.PermissionManageMessages)
+	PermissionEmbedLinks            = Permissions(discordgo.PermissionEmbedLinks)
+	PermissionAttachFiles           = Permissions(discordgo.PermissionAttachFiles)
+	PermissionReadMessageHistory    = Permissions(discordgo.PermissionReadMessageHistory)
+	PermissionMentionEveryone       = Permissions(discordgo.PermissionMentionEveryone)
+	PermissionUseExternalEmojis     = Permissions(discordgo.PermissionUseExternalEmojis)
+	PermissionUseSlashCommands      = Permissions(discordgo.PermissionUseSlashCommands)
+	PermissionManageThreads         = Permissions(discordgo.PermissionManageThreads)
+	PermissionCreatePublicThreads   = Permissions(discordgo.PermissionCreatePublicThreads)
+	PermissionCreatePrivateThreads  = Permissions(discordgo.PermissionCreatePrivateThreads)
+	PermissionUseExternalStickers   = Permissions(discordgo.PermissionUseExternalStickers)
+	PermissionSendMessagesInThreads = Permissions(discordgo.PermissionSendMessagesInThreads)
+	PermissionAddReactions          = Permissions(discordgo.PermissionAddReactions)
+
+	// Voice channel permissions
+	PermissionVoiceConnect         = Permissions(discordgo.PermissionVoiceConnect)
+	PermissionVoiceSpeak           = Permissions(discordgo.PermissionVoiceSpeak)
+	PermissionVoiceMuteMembers     = Permissions(discordgo.PermissionVoiceMuteMembers)
+	PermissionVoiceDeafenMembers   = Permissions(discordgo.PermissionVoiceDeafenMembers)
+	PermissionVoiceMoveMembers     = Permissions(discordgo.PermissionVoiceMoveMembers)
+	PermissionVoiceUseVAD          = Permissions(discordgo.PermissionVoiceUseVAD)
+	PermissionVoicePrioritySpeaker = Permissions(discordgo.PermissionVoicePrioritySpeaker)
+	PermissionVoiceStreamVideo     = Permissions(discordgo.PermissionVoiceStreamVideo)
+	PermissionUseActivities        = Permissions(discordgo.PermissionUseActivities)
+
+	// Management permissions
+	PermissionChangeNickname      = Permissions(discordgo.PermissionChangeNickname)
+	PermissionManageNicknames     = Permissions(discordgo.PermissionManageNicknames)
+	PermissionManageRoles         = Permissions(discordgo.PermissionManageRoles)
+	PermissionManageWebhooks      = Permissions(discordgo.PermissionManageWebhooks)
+	PermissionManageEmojis        = Permissions(discordgo.PermissionManageEmojis)
+	PermissionManageEvents        = Permissions(discordgo.PermissionManageEvents)
+	PermissionCreateInstantInvite = Permissions(discordgo.PermissionCreateInstantInvite)
+	PermissionKickMembers         = Permissions(discordgo.PermissionKickMembers)
+	PermissionBanMembers          = Permissions(discordgo.PermissionBanMembers)
+	PermissionAdministrator       = Permissions(discordgo.PermissionAdministrator)
+	PermissionManageChannels      = Permissions(discordgo.PermissionManageChannels)
+	PermissionManageServer        = Permissions(discordgo.PermissionManageServer)
+	PermissionViewAuditLogs       = Permissions(discordgo.PermissionViewAuditLogs)
+	PermissionViewGuildInsights   = Permissions(discordgo.PermissionViewGuildInsights)
+	PermissionModerateMembers     = Permissions(discordgo.PermissionModerateMembers)
+)
+
+func CombinePermissions(perms ...Permissions) Permissions {
+	var p Permissions
+	for _, perm := range perms {
+		p |= perm
+	}
+	return p
+}
+
+func (p Permissions) Add(perms ...Permissions) Permissions {
+	out := p
+	for _, perm := range perms {
+		out |= perm
+	}
+	return out
+}
+
+func (p Permissions) Has(perm Permissions) bool {
+	return p&perm == perm
+}
+
+// Responses
 type File struct {
 	Name        string
 	ContentType string
@@ -112,6 +189,7 @@ func (r *Response) File(f File) *Response {
 	return r
 }
 
+// Users
 type UserFlag int
 
 const (
@@ -142,12 +220,14 @@ type User struct {
 	BannerURL     string
 
 	// Guild user options
-	Guild          bool // Whether the following fields are filled out or not
+	Guild          *string // Whether the following fields are filled out or not (if nil, then no, otherwise it is the Guild ID)
 	JoinedAt       time.Time
 	Nickname       string
-	Deaf           bool   // Deafened on guild level
-	Mute           bool   // Muted on guild level
-	GuildAvatarURL string // Blank if no custom avatar
+	Deaf           bool     // Deafened on guild level
+	Mute           bool     // Muted on guild level
+	GuildAvatarURL string   // Blank if no custom avatar
+	Roles          []string // IDs of roles
+	Permissions    Permissions
 
 	userflags UserFlag
 }
@@ -158,6 +238,12 @@ func (u *User) Avatar(ctx Ctx) (image.Image, error) {
 }
 func (u *User) HasFlag(flag UserFlag) bool {
 	return u.userflags&flag != 0
+}
+func (u *User) GiveRole(c Ctx, roleID string) error {
+	return c.session().GuildMemberRoleAdd(*u.Guild, u.ID, roleID)
+}
+func (u *User) RemoveRole(c Ctx, roleID string) error {
+	return c.session().GuildMemberRoleRemove(*u.Guild, u.ID, roleID)
 }
 
 func userFromUser(d *discordgo.User) *User {
@@ -176,17 +262,20 @@ func userFromUser(d *discordgo.User) *User {
 
 func userFromMember(d *discordgo.Member) *User {
 	u := userFromUser(d.User)
-	u.Guild = true
+	u.Guild = &d.GuildID
 	u.JoinedAt = d.JoinedAt
 	u.Nickname = d.Nick
 	u.Deaf = d.Deaf
 	u.Mute = d.Mute
+	u.Roles = d.Roles
+	u.Permissions = Permissions(d.Permissions)
 	if d.Avatar != "" {
 		u.GuildAvatarURL = d.AvatarURL("")
 	}
 	return u
 }
 
+// Channels
 type ChannelType int
 
 const (
@@ -265,4 +354,86 @@ func GetGuildUser(c Ctx, id string, guild string) (*User, error) {
 		return nil, err
 	}
 	return userFromMember(d), nil
+}
+
+// Roles
+
+type Role struct {
+	// Not used for creating role
+	ID       string
+	Position int  // Position in role hierarchy
+	Managed  bool // If its managed by an integration (if true cannot give/take role)
+
+	// Required to create role
+	Name        string
+	Mentionable bool
+	Hoist       bool  // Shows up seperately in member list
+	Color       Color // Hex color of role
+	Permissions Permissions
+}
+
+func roleFromDg(r *discordgo.Role) *Role {
+	return &Role{
+		ID:          r.ID,
+		Name:        r.Name,
+		Mentionable: r.Mentionable,
+		Hoist:       r.Hoist,
+		Color:       Color(r.Color),
+		Position:    r.Position,
+		Managed:     r.Managed,
+		Permissions: Permissions(r.Permissions),
+	}
+}
+
+// GetGuildRole gets a role from a guild (NOTE: This will try to get the role from the cache if possible)
+func GetGuildRole(c Ctx, id string, guild string) (*Role, error) {
+	v, err := c.session().State.Role(guild, id)
+	if err == nil {
+		return roleFromDg(v), nil
+	}
+	r, err := c.session().GuildRoles(guild)
+	if err != nil {
+		return nil, err
+	}
+	for _, v := range r {
+		if v.ID == id {
+			return roleFromDg(v), nil
+		}
+	}
+
+	return nil, errors.New("sevcord: role not found")
+}
+
+// GetGuildRoles gets a sorted list of all roles in a guild
+func GetGuildRoles(c Ctx, guild string) ([]*Role, error) {
+	r, err := c.session().GuildRoles(guild)
+	if err != nil {
+		return nil, err
+	}
+	roles := make([]*Role, len(r))
+	for i, v := range r {
+		roles[i] = roleFromDg(v)
+	}
+	sort.Slice(roles, func(i, j int) bool {
+		return roles[i].Position < roles[j].Position
+	})
+	return roles, nil
+}
+
+// CreateRole creates the role and returns the role created
+func CreateRole(c Ctx, r *Role, guild string) (*Role, error) {
+	dgr, err := c.session().GuildRoleCreate(guild)
+	if err != nil {
+		return nil, err
+	}
+	dgr, err = c.session().GuildRoleEdit(guild, dgr.ID, r.Name, int(r.Color), r.Hoist, int64(r.Permissions), r.Mentionable)
+	if err != nil {
+		return nil, err
+	}
+	return roleFromDg(dgr), nil
+}
+
+// DeleteRole deletes a role
+func DeleteRole(c Ctx, id string, guild string) error {
+	return c.session().GuildRoleDelete(guild, id)
 }
